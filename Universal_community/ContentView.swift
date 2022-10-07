cca5ffc69a352ffb884c2d435a61d18067c231b1
Deokhyun Lee <deok.lee@obf.ateneo.edu>
2022-10-07 22:20:15 +0800

    Initial Commit

---
+++ Universal_community/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Universal_community
+//
+//  Created by 이덕현 on 10/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Universal_community/Universal_communityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Universal_communityApp.swift
+//  Universal_community
+//
+//  Created by 이덕현 on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Universal_communityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
